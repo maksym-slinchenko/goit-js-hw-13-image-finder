5aa640634cf539bd2b8723acb190855445677a45
Maksym Slinchenko <max.slin777@gmail.com>
2020-12-06 02:57:09 +0200

    start

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import "./css/styles.css";
+import parsCountriesList from "./temlates/countries-list";
+// import "@pnotify/core/dist/BrightTheme.css";
+// import "@pnotify/core/dist/PNotify.css";
+
+// Библиотеки
+
+// Переменная API
+
+//DOM-элементы
+
+// Вызов фукции
+
+// Функция
